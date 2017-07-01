3e80e0454e30dd904c705873cab190873d5b7196
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-07-01 20:48:34 +0200

    Tests for ValidatorGrid introduced

---
+++ ValidatorGrid/ValidatorGrid.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include "ValidatorGrid.hpp"
+#include "Grid.hpp"
+
+TEST(Test, ValidatorGridBothPlayersWon)
+{
+	std::string boarda = "0000000";
+	std::string boardb = "0000000";
+	std::string boardc = "0000102";
+	std::string boardd = "0000102";
+	std::string boarde = "0000102";
+	std::string boardf = "0000122";
+
+	std::string board = boardf + boarde + boardd 
+		+ boardc + boardb + boarda;
+
+	ValidatorGrid valGrid(board);
+
+	ASSERT_FALSE(valGrid.isValid());
+}
+
+TEST(Test, ValidatorGridOnePlayerHasTwoStonesMore)
+{
+	std::string boarda = "0000000";
+	std::string boardb = "0000000";
+	std::string boardc = "0000000";
+	std::string boardd = "0000102";
+	std::string boarde = "0000102";
+	std::string boardf = "1101122";
+
+	std::string board = boardf + boarde + boardd 
+		+ boardc + boardb + boarda;
+
+	ValidatorGrid valGrid(board);
+
+//	ASSERT_FALSE(valGrid.isValid());
+}
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
